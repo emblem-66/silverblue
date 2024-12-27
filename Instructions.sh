9173855daac4a04deb1e785da685bd41feafffc6
Emblem-66 <143352594+Emblem-66@users.noreply.github.com>
2024-12-27 10:02:03 +0100

    Update Instructions.sh

--- Instructions.sh
+++ Instructions.sh
@@ -10,3 +10,37 @@ source <(curl -s "https://raw.githubusercontent.com/Emblem-66/functions/refs/hea
 
 ### Run the functions
 
+### Terra repos
+f_terra
+### RPM-fusion
+f_rpmfusion
+### Fedora auto updates
+f_updates
+### Flatpak auto updates
+f_flatpak
+### First boot setup
+f_firstboot
+### Multimedia
+f_multimedia
+### Firefox
+f_firefox
+### Fonts
+f_fonts
+### CachyOS Kernel
+f_cachy
+### Mesa-git Mesa Freeworld
+#f_mesa-freeworld
+### Mesa-git Mesa Freeworld
+f_mesa-git
+### Gaming
+f_gaming
+### Utils
+f_utils
+### GNOME
+f_gnome
+### Tailscale
+f_tailscale
+### Distrobox
+#f_distrobox
+### Sublime Text
+#f_sublime
