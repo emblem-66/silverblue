60ea5e23d312db521717fda4f3aba4532b3589ad
Emblem-66 <143352594+Emblem-66@users.noreply.github.com>
2024-12-27 09:29:06 +0100

    Update Instructions.sh

--- Instructions.sh
+++ Instructions.sh
@@ -2,6 +2,8 @@
 
 set -ouex pipefail
 
+FLATPAK_PACKAGE_LIST_URL="https://raw.githubusercontent.com/Emblem-66/Silverblue/refs/heads/main/flatpak-apps.list"
+
 ### Import the functions
 
 source <(curl -s "https://raw.githubusercontent.com/Emblem-66/functions/refs/heads/main/fedora-ostree.sh")
