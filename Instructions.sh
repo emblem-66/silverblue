0938c080077e392d5d81ecd111b882061fb67636
Emblem-66 <143352594+Emblem-66@users.noreply.github.com>
2024-12-27 09:07:45 +0100

    Create Instructions.sh

---
+++ Instructions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+### Import the functions
+
+source <(curl -s "https://raw.githubusercontent.com/Emblem-66/functions/refs/heads/main/fedora-ostree.sh")
+
+### Run the functions
+
