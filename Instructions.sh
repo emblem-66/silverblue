3abdb3f93fd9f1894f0db0ff6b72500bd13c65b0
Emblem-66 <143352594+Emblem-66@users.noreply.github.com>
2025-01-19 15:32:07 +0100

    Update Instructions.sh

--- Instructions.sh
+++ Instructions.sh
@@ -4,6 +4,11 @@ set -ouex pipefail
 
 FLATPAK_PACKAGE_LIST_URL="https://raw.githubusercontent.com/Emblem-66/Silverblue/refs/heads/main/flatpak-apps.list"
 
+
+dnf install -y gdm gnome-shell ptyxis nautilus xdg-user-dirs xdg-user-dirs-gtk bash-completion 
+systemctl enable gdm
+systemctl set-default graphical.target
+
 ### Import the functions
 
 source <(curl -s "https://raw.githubusercontent.com/Emblem-66/functions/refs/heads/main/fedora-ostree.sh")
