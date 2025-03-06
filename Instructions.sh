5d463b2b83232402b945f4517e6ba85cec7b9058
emblem-66 <143352594+emblem-66@users.noreply.github.com>
2025-03-06 15:29:16 +0100

    Update Instructions.sh

--- Instructions.sh
+++ Instructions.sh
@@ -2,11 +2,11 @@
 
 set -ouex pipefail
 
-### Fedora auto updates
+# DNF auto updates
 sed -i 's/#AutomaticUpdatePolicy=none/AutomaticUpdatePolicy=stage/' /etc/rpm-ostreed.conf
 systemctl enable rpm-ostreed-automatic.timer
 
-### Flatpak auto updates
+# Flatpak setup
 #original#echo -e "[Unit]\nDescription=Update Flatpaks\n[Service]\nType=oneshot\nExecStart=/usr/bin/flatpak uninstall --unused -y --noninteractive ; /usr/bin/flatpak update -y --noninteractive ; /usr/bin/flatpak repair\n[Install]\nWantedBy=default.target\n" | tee /etc/systemd/system/flatpak-update.service
 echo -e "[Unit]\nDescription=Update Flatpaks\n[Service]\nType=oneshot\nExecStart=/usr/bin/flatpak remote-modify --disable fedora ; /usr/bin/flatpak remote-modify --enable flathub ; /usr/bin/flatpak uninstall --unused -y --noninteractive ; /usr/bin/bash -c 'curl -sSL https://raw.githubusercontent.com/emblem-66/Silverblue/refs/heads/main/flatpak-apps.list | xargs -r flatpak install -y --noninteractive' ; /usr/bin/flatpak update -y --noninteractive ; /usr/bin/flatpak repair\n[Install]\nWantedBy=default.target\n" | tee /etc/systemd/system/flatpak-update.service
 #notification#echo -e "[Unit]\nDescription=Update Flatpaks\n[Service]\nType=oneshot\nExecStart=/bin/bash -c export DISPLAY=:0 ; export XDG_RUNTIME_DIR=/run/user/$(id -u) ; /bin/bash -c export DBUS_SESSION_BUS_ADDRESS=unix:path=$XDG_RUNTIME_DIR/bus ; /usr/bin/flatpak remote-modify --disable fedora ; /usr/bin/flatpak remote-modify --enable flathub ; /usr/bin/flatpak uninstall --unused -y --noninteractive ; /usr/bin/bash -c 'curl -sSL https://raw.githubusercontent.com/emblem-66/Silverblue/refs/heads/main/flatpak-apps.list | xargs -r flatpak install -y --noninteractive' ; /usr/bin/flatpak update -y --noninteractive ; /usr/bin/flatpak repair\n/bin/bash -c notify-send "Flatpak Update" "Updates completed successfully."\n[Install]\nWantedBy=default.target\n" | tee /etc/systemd/system/flatpak-update.service
@@ -14,16 +14,16 @@ systemctl enable flatpak-update.service
 echo -e "[Unit]\nDescription=Update Flatpaks\n[Timer]\nOnCalendar=*:0/4\nPersistent=true\n[Install]\nWantedBy=timers.target\n" | tee /etc/systemd/system/flatpak-update.timer
 systemctl enable flatpak-update.timer
 
-### Remove packages
+# DNF Remove packages
 curl -sSL https://raw.githubusercontent.com/emblem-66/Silverblue/refs/heads/main/dnf-remove.list | xargs -r dnf remove -y
 
-### Install packages
+# DNF Install packages
 curl -sSL https://raw.githubusercontent.com/emblem-66/Silverblue/refs/heads/main/dnf-install.list | xargs -r dnf install -y
 
-### Firefox
+# Firefox
 #dnf remove -y firefox*
 
-### GNOME
+# GNOME
 #dnf remove -y gnome-shell-extension*
 #dnf remove -y gnome-tour
 #dnf remove -y yelp*
