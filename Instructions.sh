6e8fdf4a84e4e6aa3a93fd301a78087b531b719b
emblem-66 <143352594+emblem-66@users.noreply.github.com>
2025-04-12 11:00:38 +0200

    Update Instructions.sh

--- Instructions.sh
+++ Instructions.sh
@@ -15,7 +15,7 @@ set -ouex pipefail
 # Config files
 curl -s https://raw.githubusercontent.com/Emblem-66/Silverblue/refs/heads/main/ConfigFiles.sh | bash
 
-# Tailscale
+# Tailscale repo add
 dnf config-manager addrepo --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
 
 # COPR repo add
@@ -33,5 +33,6 @@ curl -sSL https://raw.githubusercontent.com/emblem-66/Silverblue/refs/heads/main
 # COPR repo remove
 curl -sSL https://raw.githubusercontent.com/emblem-66/Silverblue/refs/heads/main/dnf-copr.list | xargs -r dnf copr remove -y
 
+# Tailscale repo remove
 rm /etc/yum.repos.d/tailscale.repo
 
