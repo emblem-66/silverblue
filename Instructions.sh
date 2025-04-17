8cb4e42b7be146f9446a41940779c70070609297
emblem-66 <143352594+emblem-66@users.noreply.github.com>
2025-04-17 08:26:33 +0200

    Update Instructions.sh

--- Instructions.sh
+++ Instructions.sh
@@ -2,16 +2,6 @@
 
 set -ouex pipefail
 
-# DNF auto updates
-#sed -i 's/#AutomaticUpdatePolicy=none/AutomaticUpdatePolicy=stage/' /etc/rpm-ostreed.conf
-#systemctl enable rpm-ostreed-automatic.timer
-
-# Flatpak setup
-#echo -e "[Unit]\nDescription=Update Flatpaks\n[Service]\nType=oneshot\nExecStart=/usr/bin/flatpak remote-modify --disable fedora ; /usr/bin/flatpak remote-modify --enable flathub ; /usr/bin/flatpak uninstall --unused -y --noninteractive ; /usr/bin/bash -c 'curl -sSL https://raw.githubusercontent.com/emblem-66/Silverblue/refs/heads/main/flatpak-apps.list | xargs -r flatpak install -y --noninteractive' ; /usr/bin/flatpak update -y --noninteractive\n[Install]\nWantedBy=default.target\n" | tee /etc/systemd/system/flatpak-update.service
-#systemctl enable flatpak-update.service
-#echo -e "[Unit]\nDescription=Update Flatpaks\n[Timer]\nOnCalendar=*:0/4\nPersistent=true\n[Install]\nWantedBy=timers.target\n" | tee /etc/systemd/system/flatpak-update.timer
-#systemctl enable flatpak-update.timer
-
 # Config files
 curl -s https://raw.githubusercontent.com/Emblem-66/Silverblue/refs/heads/main/ConfigFiles.sh | bash
 
