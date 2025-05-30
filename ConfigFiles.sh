fe926161244cc16a9be4f5fef4a2b2f9860b48fd
emblem-66 <143352594+emblem-66@users.noreply.github.com>
2025-05-30 18:10:53 +0200

    Update ConfigFiles.sh

--- ConfigFiles.sh
+++ ConfigFiles.sh
@@ -8,3 +8,5 @@ sed -i 's/#AutomaticUpdatePolicy=none/AutomaticUpdatePolicy=stage/' /etc/rpm-ost
 # Flatpak setup
 echo -e "[Unit]\nDescription=Update Flatpaks\n[Service]\nType=oneshot\nExecStart=/usr/bin/flatpak remote-modify --disable fedora ; /usr/bin/flatpak remote-modify --enable flathub ; /usr/bin/flatpak uninstall --unused -y --noninteractive ; /usr/bin/bash -c 'curl -sSL https://raw.githubusercontent.com/emblem-66/Silverblue/refs/heads/main/flatpak-apps.list | xargs -r flatpak install -y --noninteractive' ; /usr/bin/flatpak update -y --noninteractive\n[Install]\nWantedBy=default.target\n" | tee /etc/systemd/system/flatpak-update.service
 echo -e "[Unit]\nDescription=Update Flatpaks\n[Timer]\nOnCalendar=*:0/4\nPersistent=true\n[Install]\nWantedBy=timers.target\n" | tee /etc/systemd/system/flatpak-update.timer
+
+echo -e "{\n“BraveRewardsDisabled”: true,\n“BraveWalletDisabled”: true,\n“BraveVPNDisabled”: 1,\n“BraveAIChatEnabled”: false,\n“TorDisabled”: true,\n}\n" | tee /etc/brave/policies/managed/test_policy.json
