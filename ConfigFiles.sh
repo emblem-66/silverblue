3919841ba98c501ee8721fe61f27c2e3e438f72a
emblem-66 <143352594+emblem-66@users.noreply.github.com>
2025-07-12 15:53:17 +0200

    Update ConfigFiles.sh

--- ConfigFiles.sh
+++ ConfigFiles.sh
@@ -92,7 +92,7 @@ git clone https://github.com/somepaulo/MoreWaita.git /usr/share/icons/MoreWaita/
 #systemctl enable httpd.service || true
 
 # rpm-fusion
-#dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 #dnf install -y --allowerasing libavcodec-freeworld mesa*freeworld ffmpeg gstreamer*
 
