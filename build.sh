a1c0b23acd27be3f2c469a44222bd86a5643a9d4
emblem-66 <143352594+emblem-66@users.noreply.github.com>
2025-10-07 19:15:22 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -21,15 +21,15 @@ curl --create-dirs -o /usr/lib/systemd/system/brew-setup.service https://raw.git
 curl --create-dirs -o /usr/lib/systemd/system/brew-update.service https://raw.githubusercontent.com/emblem-66/bootc-config/refs/heads/main/_usr_lib_systemd_system_brew-update.service
 curl --create-dirs -o /usr/lib/systemd/system/brew-packages.service https://raw.githubusercontent.com/emblem-66/bootc-config/refs/heads/main/_usr_lib_systemd_system_brew-packages.service
 # wallpaper
-curl --create-dirs -o /usr/share/backgrounds/w1.jpg https://w.wallhaven.cc/full/w5/wallhaven-w5emlr.jpg
-curl --create-dirs -o /usr/share/backgrounds/w2.jpg https://w.wallhaven.cc/full/3l/wallhaven-3lr2p6.jpg
-curl --create-dirs -o /usr/share/backgrounds/w3.jpg https://w.wallhaven.cc/full/3k/wallhaven-3k73r6.png
-curl --create-dirs -o /usr/share/backgrounds/w4.jpg https://w.wallhaven.cc/full/vm/wallhaven-vm2z55.png
-curl --create-dirs -o /usr/share/backgrounds/w5.jpg https://w.wallhaven.cc/full/2k/wallhaven-2k6z39.png
-curl --create-dirs -o /usr/share/backgrounds/w6.jpg https://w.wallhaven.cc/full/zx/wallhaven-zxv86v.jpg
-curl --create-dirs -o /usr/share/backgrounds/w7.jpg https://w.wallhaven.cc/full/eo/wallhaven-eov7lw.jpg
-curl --create-dirs -o /usr/share/backgrounds/w8.jpg https://w.wallhaven.cc/full/9d/wallhaven-9dkprw.png
-curl --create-dirs -o /usr/share/backgrounds/w9.jpg https://w.wallhaven.cc/full/6d/wallhaven-6de6wl.png
+curl --create-dirs -o /usr/share/backgrounds/gnome/w1.jpg https://w.wallhaven.cc/full/w5/wallhaven-w5emlr.jpg
+curl --create-dirs -o /usr/share/backgrounds/gnome/w2.jpg https://w.wallhaven.cc/full/3l/wallhaven-3lr2p6.jpg
+curl --create-dirs -o /usr/share/backgrounds/gnome/w3.jpg https://w.wallhaven.cc/full/3k/wallhaven-3k73r6.png
+curl --create-dirs -o /usr/share/backgrounds/gnome/w4.jpg https://w.wallhaven.cc/full/vm/wallhaven-vm2z55.png
+curl --create-dirs -o /usr/share/backgrounds/gnome/w5.jpg https://w.wallhaven.cc/full/2k/wallhaven-2k6z39.png
+curl --create-dirs -o /usr/share/backgrounds/gnome/w6.jpg https://w.wallhaven.cc/full/zx/wallhaven-zxv86v.jpg
+curl --create-dirs -o /usr/share/backgrounds/gnome/w7.jpg https://w.wallhaven.cc/full/eo/wallhaven-eov7lw.jpg
+curl --create-dirs -o /usr/share/backgrounds/gnome/w8.jpg https://w.wallhaven.cc/full/9d/wallhaven-9dkprw.png
+curl --create-dirs -o /usr/share/backgrounds/gnome/w9.jpg https://w.wallhaven.cc/full/6d/wallhaven-6de6wl.png
 
 ### Packages
 # Tailscale
