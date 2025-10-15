193ba6aa35d73e884ca9655d7b47302c60250e9d
emblem-66 <143352594+emblem-66@users.noreply.github.com>
2025-10-15 16:36:22 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -58,6 +58,10 @@ dnf install -y piper
 
 dnf install -y tuned-gtk bootc-gtk
 
+# FTP
+dnf install -y vsftpd
+systemctl enable vsftpd
+
 # Cosmic
 #curl --create-dirs -o /etc/yum.repos.d/cosmic.repo https://copr.fedorainfracloud.org/coprs/ryanabx/cosmic-epoch/repo/fedora-$(rpm -E %fedora)/ryanabx/cosmic-epoch-fedora-$(rpm -E %fedora).repo
 #dnf install -y cosmic-desktop && rm -rf /etc/yum.repos.d/cosmic.repo
