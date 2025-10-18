180c90f126a43f481b61a97c9e0304f5acb620a4
emblem-66 <143352594+emblem-66@users.noreply.github.com>
2025-10-18 18:12:55 +0200

    Fix typo in podlet installation command

--- build.sh
+++ build.sh
@@ -63,6 +63,9 @@ dnf install -y piper
 #systemctl enable vsftpd
 #chown 0777 /etc/vsftpd/vsftpd.conf
 
+# podlet
+dnf isntall -y podlet
+
 # docker
 dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
