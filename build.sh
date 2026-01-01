77f0a239036659b62f2ae377b3a66c4c146285f8
emblem-66 <143352594+emblem-66@users.noreply.github.com>
2026-01-01 10:50:55 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -xeuo pipefail
 
-
-dnf install -y \
-  https://repos.fyralabs.com/terra$(rpm -E %fedora)/terra-release-mesa.rpm \
-  https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
-  https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-
-dnf config-manager setopt terra.priority=3
-dnf config-manager setopt fedora.exclude="mesa-*"
-dnf config-manager setopt rpmfusion.exclude="mesa-*"
 
 
 
