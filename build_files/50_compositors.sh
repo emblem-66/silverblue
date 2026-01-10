a3f539f0ef7e65faef381d5918ee5d86f70484e9
emblem-66 <143352594+emblem-66@users.noreply.github.com>
2026-01-10 20:52:35 +0100

    Update 50_compositors.sh

--- build_files/50_compositors.sh
+++ build_files/50_compositors.sh
@@ -1,1 +1,64 @@
+#!/usr/bin/env bash
+set -xeuo pipefail
+
+echo "::group::50"
+trap 'echo "::endgroup::"' EXIT
+
+#dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+### Utils
+dnf install -y \
+    kitty \
+    wofi \
+    waybar \
+    waypaper \
+    pavucontrol \
+    blueman \
+    mako \
+    wlogout \
+    waybar \
+    swww \
+    grim \
+
+### Hyprland
+#curl -sS --create-dirs -o /etc/yum.repos.d/_hyprland.repo https://copr.fedorainfracloud.org/coprs/sdegler/hyprland/repo/fedora-$(rpm -E %fedora)/sdegler-hyprland-fedora-$(rpm -E %fedora).repo
+dnf copr enable sdegler/hyprland
+dnf install -y \
+    hyprutils \
+    hyprland-guiutils \
+    hyprland-autoname-workspaces \
+    hyprpaper \
+    hyprlock \
+    hypridle \
+    hyprlauncher \
+    hyprnome \
+    hyprshot \
+    hyprsysteminfo \
+    hyprwire \
+    hyprland-plugins \
+    hyprshot \
+    hyprland-qt-support \
+    hyprcursor \
+
+### Sway
+dnf install -y \
+    sway \
+    swaybg \
+    swaylock \
+    swayidle \
+
+### MangoWC
+dnf install -y --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
+dnf install -y mangowc
+
+### Niri
+dnf copr enable avengemedia/dms
+dnf install -y niri
+
+### DMS
+#curl -sS --create-dirs -o /etc/yum.repos.d/_dms.repo https://copr.fedorainfracloud.org/coprs/avengemedia/dms/repo/fedora-$(rpm -E %fedora)/avengemedia-dms-fedora-$(rpm -E %fedora).repo
+dnf copr enable avengemedia/dms
+dnf install -y \
+    dms \
+    dms-greeter \
 
