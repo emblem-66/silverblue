ac3f143e6a9e512d853e6aeaa058ff64692d089d
emblem-66 <143352594+emblem-66@users.noreply.github.com>
2026-01-10 17:40:45 +0100

    Update 90_extra.sh

--- build_files/90_extra.sh
+++ build_files/90_extra.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
+echo "::group::Checkup"
+trap 'echo "::endgroup::"' EXIT
 
 dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
