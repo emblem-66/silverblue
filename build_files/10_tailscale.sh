2eb14d043604853df14dcec20dbaf1b865021c68
emblem-66 <143352594+emblem-66@users.noreply.github.com>
2026-01-10 17:23:31 +0100

    Create 10_tailscale.sh

---
+++ build_files/10_tailscale.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -xeuo pipefail
+
+echo "::group::Executing build-gnome-extensions"
+trap 'echo "::endgroup::"' EXIT
+
+curl -sS --create-dirs -o /etc/yum.repos.d/_tailscale.repo https://pkgs.tailscale.com/stable/fedora/tailscale.repo
+
+dnf install -y tailscale
+
+systemctl enable tailscaled.service
+
+systemctl enable sshd.service
+
