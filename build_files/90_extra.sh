35e69edc6003d0af019ad6b57abfe5addc13da16
emblem-66 <143352594+emblem-66@users.noreply.github.com>
2026-01-17 13:31:40 +0100

    Update 90_extra.sh

--- build_files/90_extra.sh
+++ build_files/90_extra.sh
@@ -10,3 +10,16 @@ dnf remove -y glibc-all-langpacks
 
 dnf copr enable -y che/nerd-fonts
 dnf install -y nerd-fonts
+
+dnf install -y \
+    adwaita-fonts-all \
+    ibm-plex-fonts-all \
+    mozilla-zilla-slab-fonts \
+    cascadia-fonts-all \
+    rbanffy-3270-fonts \
+    adobe-source-sans-pro-fonts \
+    adobe-source-serif-pro-fonts \
+    adobe-source-mono-pro-fonts \
+    jetbrains-mono-fonts-all \
+    fontawesome-fonts-all \
+    
