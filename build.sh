950b18e3140ec7f0cbe2e480edc1ef6a14f3e7f1
emblem-66 <143352594+emblem-66@users.noreply.github.com>
2026-02-03 12:16:21 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,11 +10,15 @@ dnf config-manager addrepo --from-repofile=https://github.com/terrapkg/subatomic
 #dnf install -y --enablerepo='tailscale-stable' tailscale
 dnf install -y terra-release
 # Tailscale
-dnf config-manager addrepo --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
-dnf config-manager setopt tailscale-stable.enabled=0
-dnf install -y --enablerepo='tailscale-stable' tailscale
-systemctl enable tailscaled
-systemctl enable sshd.service
+#dnf config-manager addrepo --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
+#dnf config-manager setopt tailscale-stable.enabled=0
+#dnf install -y --enablerepo='tailscale-stable' tailscale
+#systemctl enable tailscaled
+#systemctl enable sshd.service
+
+#
+dnf install -y --nogpgcheck --repofrompath 'tailscale,https://pkgs.tailscale.com/stable/fedora/tailscale.repo' tailscale
+  
 # Just
 dnf install -y just
 
