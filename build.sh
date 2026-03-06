a06bdd68c0baa960f0e584f91da312af900ca6a0
emblem-66 <143352594+emblem-66@users.noreply.github.com>
2026-03-06 21:52:23 +0100

    Add system and user service management to build.sh

--- build.sh
+++ build.sh
@@ -161,3 +161,45 @@ flatpak remote-modify --system --disable fedora
 #rm -rf /etc/yum.repos.d/_*.repo
 
 #systemctl list-unit-files --state=enabled
+
+
+system_services=(
+  bootc-fetch-apply-updates.service
+  tailscaled.service
+  cockpit.socket
+  btrfs-scrub.timer
+  podman-auto-update.timer
+)
+
+user_services=(
+  podman.socket
+  flathub-setup.service
+)
+
+mask_services=(
+  logrotate.service
+  logrotate.timer
+  akmods-keygen.target
+  rpm-ostree-countme.timer
+  rpm-ostree-countme.service
+  systemd-remount-fs.service
+  flatpak-add-fedora-repos.service
+  NetworkManager-wait-online.service
+  akmods-keygen@akmods-keygen.service
+)
+
+systemctl enable "${system_services[@]}"
+systemctl mask "${mask_services[@]}"
+systemctl --global enable "${user_services[@]}"
+
+for service in "${system_services[@]}"; do
+    echo "enable $service" >> "$preset_file"
+done
+
+mkdir -p "/etc/systemd/user-preset/"
+preset_file="/etc/systemd/user-preset/01-zena.preset"
+touch "$preset_file"
+
+for service in "${user_services[@]}"; do
+    echo "enable $service" >> "$preset_file"
+done
