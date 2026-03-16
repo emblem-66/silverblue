3a04c4f76db04fff93ebd3a68769c31f85fdced5
emblem-66 <143352594+emblem-66@users.noreply.github.com>
2026-03-16 09:47:00 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
+# --setopt=install_weak_deps=False
+
 dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 dnf config-manager setopt fedora-cisco-openh264.enabled=1
 
