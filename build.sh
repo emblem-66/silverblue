b5daa43ca827780ac38c60bff239aa6a88133db1
emblem-66 <143352594+emblem-66@users.noreply.github.com>
2026-03-20 19:27:53 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,29 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
+
+
+
+
+
+dnf -y install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release{,-extras,-mesa} 
+dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf -y config-manager setopt "*terra*".priority=3 #"*terra*".exclude="nerd-fonts topgrade scx-tools scx-scheds steam python3-protobuf zlib-devel"
+#dnf -y config-manager setopt "terra-mesa".enabled=false
+dnf -y config-manager setopt "*rpmfusion*".priority=5 "*rpmfusion*".exclude="mesa-*"
+dnf -y config-manager setopt "*fedora*".exclude="mesa-* kernel-core-* kernel-modules-* kernel-uki-virt-*"
+
+
 # --setopt=install_weak_deps=False
 
-dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-dnf config-manager setopt fedora-cisco-openh264.enabled=1
+#dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+#dnf config-manager setopt fedora-cisco-openh264.enabled=1
 
 #dnf config-manager setopt fedora-cisco-openh264.enabled=0
 
-dnf config-manager addrepo --from-repofile=https://github.com/terrapkg/subatomic-repos/raw/main/terra.repo
+#dnf config-manager addrepo --from-repofile=https://github.com/terrapkg/subatomic-repos/raw/main/terra.repo
 #sed -i '0,/enabled=0/s/enabled=0/enabled=1/' /etc/yum.repos.d/terra-mesa.repo
-dnf config-manager setopt terra.enabled=1
+#dnf config-manager setopt terra.enabled=1
 #dnf install -y --enablerepo='terra' terra-release
 #dnf install -y --enablerepo='terra' terra-release-extras
 #dnf install -y --enablerepo='terra' terra-release-mesa
@@ -18,11 +31,11 @@ dnf config-manager setopt terra.enabled=1
 #dnf install -y --enablerepo='terra' terra-release-multimedia
 
 #dnf install -y terra-release*
-dnf install -y terra-release
-dnf install -y terra-release-extras
-dnf install -y terra-release-mesa
-dnf install -y terra-release-nvidia
-dnf install -y terra-release-multimedia
+#dnf install -y terra-release
+#dnf install -y terra-release-extras
+#dnf install -y terra-release-mesa
+#dnf install -y terra-release-nvidia
+#dnf install -y terra-release-multimedia
 
 #dnf swap mesa-va-drivers-freeworld mesa-va-drivers
 #dnf install -y mangowm noctalia-shell
@@ -64,6 +77,9 @@ dnf install -y \
 # Screen brightness
 dnf install -y ddcutil
 
+# Debug
+dnf install -y evtest
+
 # Cockpit
 dnf install -y cockpit cockpit-podman
 
@@ -97,12 +113,39 @@ dnf remove -y \
 
 dnf autoremove -y
 
+
+
 # Update tweaks
 sed -i 's|^ExecStart=.*|ExecStart=/usr/bin/bootc update --quiet|' /usr/lib/systemd/system/bootc-fetch-apply-updates.service
 sed -i 's|#AutomaticUpdatePolicy.*|AutomaticUpdatePolicy=stage|' /etc/rpm-ostreed.conf
 sed -i 's|#LockLayering.*|LockLayering=true|' /etc/rpm-ostreed.conf
 
+# Enable services
 systemctl preset-all
 systemctl --global preset-all
 
-systemctl list-unit-files --state=enabled
+
+
+
+
+
+
+
+
+
+
+
+
+dnf install -y \
+    steam \
+    steam-devices
+
+
+
+
+#dnf -y --enable-repo=terra-mesa --setopt=install_weak_deps=False install \
+#    steam \
+#    steam-devices
+
+
+dnf autoremove -y
